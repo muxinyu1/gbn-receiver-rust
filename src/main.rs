a4d510237f9fe86afaa13a28d0e9b456eae7c1c3
mxy <13614169547@163.com>
2023-04-24 00:46:28 +0800

    修正小Bug

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,20 @@ use ack::Ack;
 
 mod config;
 mod ack;
+mod packet;
 
 fn main() -> io::Result<()> {
     let config_json = std::fs::read_to_string("config.json")?;
     let config: Config = serde_json::from_str(&config_json)?;
 
+    let buffer_size = std::mem::size_of::<i32>() + std::mem::size_of::<usize>() + config.data_size() + std::mem::size_of::<u16>();
+
     let socket = UdpSocket::bind(format!("127.0.0.1:{}", config.port()))?;
     let mut seq_num = 0; // 从0开始接收分组
     // Vec的大小是发送的序列号 + 数据字节数 + 数据 + 校验和
-    let mut buffer = vec![0; std::mem::size_of::<i32>() * 2 + config.data_size() + std::mem::size_of::<i16>()];
+    let mut buffer = vec![0; buffer_size];
     let mut crc_ccitt = CRCu16::crc16ccitt_false(); // 用于计算CRC校验和
+    crc_ccitt.reset();
     loop {
         println!("等待帧序列号为{}的分组...", seq_num);
         let (n, sender) = socket.recv_from(&mut buffer)?;
@@ -33,10 +37,12 @@ fn main() -> io::Result<()> {
             continue;
         }
         let recv_checksum = u16::from_le_bytes([buffer[buffer.len() - 2], buffer[buffer.len() - 1]]);
-        let data_to_check = &buffer[..(std::mem::size_of::<i32>() * 2 + config.data_size())]; // 要计算校验码的部分
-        crc_ccitt.digest(data_to_check); // 计算校验码
+        crc_ccitt.digest(&buffer[..(buffer_size - std::mem::size_of::<u16>())]); // 计算校验码
+        let crc = crc_ccitt.get_crc(); // 保存校验码
+        crc_ccitt.reset(); // 重置crc状态
+
         if recv_checksum != crc_ccitt.get_crc() {
-            println!("校验码出错(期待的: {}, 实际收到的: {}), 丢弃", crc_ccitt.get_crc(), recv_checksum);
+            println!("校验码出错(期待的: {}, 实际收到的: {}), 丢弃", crc, recv_checksum);
             if seq_num == 0 {
                 continue;
             }
